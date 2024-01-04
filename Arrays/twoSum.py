ca356eeeab081bd0ece46fd39682585c969b719f
Shivesh Prakash <64745558+Shivesh777@users.noreply.github.com>
2024-01-04 00:31:33 -0500

    fix indents

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -28,12 +28,13 @@ def two_sum(nums: list[int], target: int) -> list[int]:
     - Space complexity: O(n) for the dictionary 'seen'.
     """
     seen = {}
-        for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in seen:
-                return [seen[diff], i]
-            else:
-                seen[nums[i]] = i
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in seen:
+            return [seen[diff], i]
+        else:
+            seen[nums[i]] = i
+            
 
 def two_sum_brute(nums: list[int], target: int) -> list[int]:
     """
